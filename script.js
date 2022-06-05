c736a824ac8ac3f82cb813f07652cc91d57e3b6d
SanteIsHere <r.a.frye00@gmail.com>
2022-06-05 15:55:07 -0400

    Create the 16x16 grid, add hover rule to each cell

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 // Get the grid div
 const grid = document.getElementById("grid")
 
-for (let i = 0; i < 257; i++) {
-    let div = document.createElement("div")
-    div.setAttribute("style", "width: 200px; height: 200px; border: 1px dashed black;")
-    grid.appendChild(div)
-    delete div
+for (let i = 0; i < 16; i++) {
+    let row = document.createElement("div")
+    row.setAttribute("style", "display: flex; border: 1px dashed black; width: 100%; height: 30px;")
+    for (let x = 0; x < 16; x++) {
+        let box = document.createElement("div")
+        // box.setAttribute("style", "width: 100%; height: 100%; border: 1px solid blue;")
+        box.classList.add("cell")
+        row.appendChild(box)
+    }
+    grid.appendChild(row)
 }
+
+let styleSheet = document.styleSheets[0]
+styleSheet.insertRule(".cell {width: 100%; height: 100%}")
+styleSheet.insertRule(".cell:hover {border: 1px solid blue}")
+// console.log(document.getElementsByClassName("cell"))
