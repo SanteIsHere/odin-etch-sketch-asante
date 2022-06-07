de04d1367e2a732c91d7f1c5a627666e571c797d
SanteIsHere <r.a.frye00@gmail.com>
2022-06-07 10:52:01 -0400

    Add reset grid function to reset button

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 function reconfigGrid() {
     // Get new grid dimensions from user prompt (convert/cast to an integer from string)
-    res = Number(prompt("Dimensions? (100 or less px plz)"))
+    res = Number(prompt("Dimensions? (dimensions <= 100 or dimensions >= 16)"))
     mainDiv.removeChild(grid)
     // If user input provided and less than or equal
-    //  to 100, createGrid with provided argument else 
+    //  to 100 and greater than or equal to 16,
+    //  createGrid with provided argument else 
     //  createGrid with default resolution (16x16)
+    
     // Remove the cell class rules before replacing in function
     styleSheet.deleteRule(styleSheet.cssRules.length-1)
     styleSheet.deleteRule(styleSheet.cssRules.length-1)
-    if (res && res <= 100) {
+    if (res && (res <= 100) && (res >= 16)) {
         createGrid(res, defaultColor);
         resDisp.textContent = `${res}x${res}`;
     } else {
@@ -32,13 +34,18 @@ function changeGridColor() {
     }
 }
 
+function resetGrid() {
+    mainDiv.removeChild(grid)
+    createGrid(defaultRes, defaultColor)
+}
+
 function createGrid(res, color) {
     // Create the grid div
     const grid = document.createElement("div")
 
     // Set the id for the grid
     grid.id = "grid"
-
+    
     // Append the grid box/div to the main div
     mainDiv.appendChild(grid)
 
@@ -57,13 +64,23 @@ function createGrid(res, color) {
 
     console.log(styleSheet.cssRules)
     
-    styleSheet.insertRule(".cell:hover {opacity: 1; transition: opacity\
-        0.2s ease; transform: scale(1.04);", cssRulesLen)
-
-    styleSheet.insertRule(`.cell {width: 100%; height: 100%; opacity: 0;\
-    transition: opacity 2s ease 0.4s, transform 0.8s ease; \ 
-    background-color: ${color}}`, cssRulesLen)
     
+    // ==== These rules are for a drawing trail effect ==== //
+    // styleSheet.insertRule(".cell:hover {opacity: 1; transition: opacity\
+    //     0.2s ease; transform: scale(1.04);", cssRulesLen)
+
+    // styleSheet.insertRule(`.cell {width: 100%; height: 100%; opacity: 0;\
+    // transition: opacity 2s ease 0.4s, transform 0.8s ease; \ 
+    // background-color: ${color}}`, cssRulesLen)
+    
+    styleSheet.insertRule(`.cell {width: 100%; height: 100%;\
+        background-color: transparent; border: 2px solid black}`, cssRulesLen)
+    
+    grid.addEventListener("mouseover", function (e) {if (e.target.matches('.cell')) {
+        e.target.classList.add('active');
+      }})
+    
+    styleSheet.insertRule(`.cell.active {background-color: ${color}}`, cssRulesLen)
     // Checking the style rules were applied
     console.log(styleSheet.cssRules)
 }
