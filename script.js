7b38bbde59822ecdf70a89678d5390c88e5707d4
SanteIsHere <r.a.frye00@gmail.com>
2022-06-07 11:13:41 -0400

    Change variable name to "cell" from "box" for cell divs, refactor reconfigGrid function

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 function reconfigGrid() {
     // Get new grid dimensions from user prompt (convert/cast to an integer from string)
-    res = Number(prompt("Dimensions? (dimensions <= 100 or dimensions >= 16)"))
+    res = Number(prompt("Dimensions? (dimensions <= 100)"))
     mainDiv.removeChild(grid)
-    // If user input provided and less than or equal
-    //  to 100 and greater than or equal to 16,
-    //  createGrid with provided argument else 
-    //  createGrid with default resolution (16x16)
     
     // Remove the cell class rules before replacing in function
     styleSheet.deleteRule(styleSheet.cssRules.length-1)
     styleSheet.deleteRule(styleSheet.cssRules.length-1)
+    
+    // If user input provided and less than or equal
+    //  to 100, create the grid with provided argument else 
+    //  create the grid with default resolution (16x16)
     if (res && (res <= 100)) {
         createGrid(res, defaultColor);
         resDisp.textContent = `${res}x${res}`;
@@ -28,7 +28,7 @@ function changeGridColor() {
     console.log(`After deleting rules:`, styleSheet.cssRules)
     if (res) {
         console.log("Okay")
-        createGrid(res, selColor);
+        createGridsolid(res, selColor);
     } else {
         createGrid(defaultRes, selColor);
     }
@@ -54,16 +54,12 @@ function createGrid(res, color) {
         let row = document.createElement("div")
         row.setAttribute("style", "display: flex; height: 100%;")
         for (let x = 0; x < res; x++) {
-            let box = document.createElement("div")
-            box.classList.add("cell")
-            row.appendChild(box);
+            let cell = document.createElement("div")
+            cell.classList.add("cell")
+            row.appendChild(cell);
         }
         grid.appendChild(row);
     }
-
-    let cssRulesLen = styleSheet.cssRules.length
-
-    console.log(styleSheet.cssRules)
     
     // ==== These rules are for a drawing trail effect ==== //
     // styleSheet.insertRule(".cell:hover {opacity: 1; transition: opacity\
@@ -74,7 +70,7 @@ function createGrid(res, color) {
     // background-color: ${color}}`, cssRulesLen)
     
     styleSheet.insertRule(`.cell {width: 100%; height: 100%;\
-        background-color: transparent; border: 0.5px solid black}`, cssRulesLen)
+        background-color: transparent; border: 0.5px dashed black}`, cssRulesLen)
     
     grid.addEventListener("mouseover", function (e) {if (e.target.matches('.cell')) {
         e.target.classList.add('active');
@@ -87,6 +83,9 @@ function createGrid(res, color) {
 
 // Get the style sheet
 const styleSheet = document.styleSheets[0]
+
+// Acquire the length of the cssRules in the style sheet
+const cssRulesLen = styleSheet.cssRules.length
 
 // Get the main div
 const mainDiv = document.getElementById("main")
